a51013029569c8f631b715ad9333d6bc05255232
JosuedGarm52 <jaccobjosue@gmail.com>
2024-06-26 14:35:50 -0500

    Cambio de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.1.1"
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.4.0"
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 android {
